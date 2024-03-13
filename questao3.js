3ed8d0b1615e8de7a9bff863972c64148e22f7d6
thaua-arruda <thauafarruda@gmail.com>
2024-03-13 16:46:44 -0400

    questão 3  resolvida

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript? 
+*/
+
+let nome ="Thauã"
+let sobrenome = "Arruda"
+let resultado;
+
+resultado = nome +" "+ sobrenome
+console.log(resultado);
