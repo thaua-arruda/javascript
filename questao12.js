899b120d19ddf3c6cb949fa841e49bd4a37b9aa0
thaua-arruda <thauafarruda@gmail.com>
2024-03-18 16:59:32 -0400

    adicionado questao 12

---
+++ questao12.js
@@ -0,0 +1,11 @@
+/*
+Questão 12: Como você pode iterar por meio de um array em JavaScript 
+usando um loop for?
+*/
+
+function contarAte(numero) {
+    for(let i = 1; i <= numero; i++) {
+      console.log("Contagem: ", i);
+    }
+  }
+  contarAte(100000) // Exemplo kkkkkk
