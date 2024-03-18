91001f3bceb534c3e8ee3860ba3fba088ed637ef
thaua-arruda <thauafarruda@gmail.com>
2024-03-18 14:43:57 -0400

    adicionado exemplo de objetos

---
+++ objetos.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "Thauã",
+    idade: 30,
+    profissao: "Engenheiro"
+};
+console.log(pessoa.nome); // Saída: Thauã
+console.log(pessoa.idade); //Saída: 30
+console.log(pessoa.profissao); //Saída: Engenheiro
