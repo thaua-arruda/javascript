05048ebd1879fa9dfec219de0db0920d90de424b
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 14:50:18 -0400

    adicionado questao 16

---
+++ questao16.js
@@ -0,0 +1,21 @@
+/*
+Questão 16: Como você pode criar um objeto vazio em JavaScript?
+*/
+
+const obj = {}
+
+console.log(obj) // {}
+
+const person = {
+    nome: "capa tropa"
+}
+
+console.log(person) // nome: "capa tropa"
+
+//ou
+
+/*
+const obj = new Object()
+
+console.log(obj) // {}
+*/
