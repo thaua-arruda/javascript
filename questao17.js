ebcdc40bb45fda9b414ce012d3af54b863265886
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 14:50:37 -0400

    adicionado questao 17

---
+++ questao17.js
@@ -0,0 +1,11 @@
+/*
+Questão 17: Como você pode adicionar uma propriedade a um objeto em 
+JavaScript? 
+*/
+
+const meucarro = {
+    fabricacao: "Chevrolet",
+    modelo: "Corvette",
+    ano: 1974,
+}
+console.log(meucarro);
