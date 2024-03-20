386ae4f80e896291c5d6690ca8b63d755e5688a4
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:28:27 -0400

    adicionado questao 23

---
+++ questao23.js
@@ -0,0 +1,11 @@
+/*
+Questão 23: Como você pode criar um loop while em JavaScript?
+*/
+
+let contador = 1;
+
+while (contador <= 5) {
+    console.log("Contador: " + contador);
+    contador++;
+}
+		
