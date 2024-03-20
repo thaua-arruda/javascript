5f3c7a79630a82f9e0d18762e98d6fe556809cce
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:28:53 -0400

    adicionado questao 27

---
+++ questao27.js
@@ -0,0 +1,10 @@
+/*
+Questão 27: Como você pode remover um elemento específico de um array 
+em JavaScript?
+*/
+
+var numeros = [2,5,9,14,3];
+
+numeros.splice(4, 1);
+
+console.log(numeros); // [2, 5, 9, 14]
