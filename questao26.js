fa0ed73811a2545d5abe0e85e41d954d4c9427a0
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:28:46 -0400

    adicionado questao 26

---
+++ questao26.js
@@ -0,0 +1,10 @@
+/*
+Questão 26: Como você pode usar o operador ternário em JavaScript?
+*/
+
+let idadeUsuario = 67;
+const textoConsole =
+    idadeUsuario >= 65
+        ? "Você tem direito de usufruir da gratuidade do serviço."
+        : "Apenas pessoas com 65 anos ou mais possuem direito à gratuidade do serviço.";
+console.log(textoConsole);
