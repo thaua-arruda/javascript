a6b43c82ff21b659603cf8e1962ba2201a9c4589
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 14:49:59 -0400

    adicionado questao 15

---
+++ questao15.js
@@ -0,0 +1,8 @@
+/*
+Questão 15: Como você pode obter o comprimento de uma string em 
+JavaScript?
+*/
+
+var carros = ["Civic", "Fusion", "Focus", "Jetta", "Cruze", "Corolla"];
+var tamanho = carros.length; // Saída: 6
+console.log(tamanho);
