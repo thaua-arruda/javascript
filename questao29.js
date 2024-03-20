88763c9c121eca25fa16fd86138a7e365009f382
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:29:05 -0400

    adicionado questao 29

---
+++ questao29.js
@@ -0,0 +1,9 @@
+/*
+Questão 29: Como você pode converter um número em uma string em 
+JavaScript?
+*/
+
+let stringNumero = 39;
+console.log(typeof stringNumero);
+let str = stringNumero.toString();
+console.log(typeof str);
