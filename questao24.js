6f250040d19d7dc332cbc38bbec0f310bacccf33
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:28:33 -0400

    adicionado questao 24

---
+++ questao24.js
@@ -0,0 +1,11 @@
+/*
+Questão 24: Como você pode adicionar um evento de clique a um elemento 
+HTML em JavaScript?
+*/
+
+const butao = document.GetElementByld("nomeTag");
+butao.addEventListener("click", minhaFuncao);
+
+function minhaFuncao(){
+    console.log("o botao foi clicado")
+}
