4bc758c8171d5f17fce7852591d1bd2160a9823f
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:28:19 -0400

    adicionado questao 22

---
+++ questao22.js
@@ -0,0 +1,11 @@
+/*
+Questão 22: Como você pode verificar se uma variável é nula em JavaScript?
+*/
+
+let variavel = null;
+
+if (variavel === null){
+    //Código a ser executado caso a variável seja nula
+}
+
+console.log(variavel);
