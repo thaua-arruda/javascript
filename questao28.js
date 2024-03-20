02b9d2f99c844310bd10063016ac1178078eb48a
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:28:59 -0400

    adicionado questao 28

---
+++ questao28.js
@@ -0,0 +1,9 @@
+/*
+Questão 28: Como você pode converter uma string em um número inteiro em 
+JavaScript?
+*/
+
+let numeroString = "57";
+let numero = parseInt(numeroString);
+
+console.log(numero);
