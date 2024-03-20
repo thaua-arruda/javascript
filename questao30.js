7dd97ae185d733475c10f7bff8f1923846a68f16
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 17:29:23 -0400

    adicionado questa0 30/finalizado

---
+++ questao30.js
@@ -0,0 +1,11 @@
+/*
+Questão 30: Como você pode verificar se uma variável é definida em 
+JavaScript?
+*/
+
+let variavel = 1;
+if(typeof variavel === "undefined") {
+    console.log("Variável não definida");
+} else {
+    console.log("Variável definida");
+}
