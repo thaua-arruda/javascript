89b94f0ffd1f2b5e15f3d46b6fcb8d6c940f13ac
thaua-arruda <thauafarruda@gmail.com>
2024-03-20 14:50:54 -0400

    adicionado questao 18

---
+++ questao18.js
@@ -0,0 +1,14 @@
+/*
+Questão 18: Como você pode acessar o valor de uma propriedade de um 
+objeto em JavaScript?
+*/
+
+const meucarro = {
+    fabricacao: "Chevrolet",
+    modelo: "Corvette",
+    ano: 1974,
+}
+console.log(meucarro.fabricacao);
+console.log(meucarro.modelo);
+console.log(meucarro.ano);
+
